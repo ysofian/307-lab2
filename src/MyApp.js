7736fdfbfc4b0935cf7d053175296198edb04ff5
ysofian <ysofian@calpoly.edu>
2023-04-08 19:12:07 -0700

    added delete feature

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Table from './Table'
+import React, {useState} from 'react';
+import Table from './Table';
 
-
-const characters = [
+function MyApp() {
+  const [characters, setCharacters] = useState([
     {
       name: 'Charlie',
       job: 'Janitor',
@@ -19,20 +19,22 @@ const characters = [
       name: 'Dennis',
       job: 'Bartender',
     },
-];
+  ]);    
 
-function MyApp() {
-    return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      );
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    </div>
+  )
+
 }
 
-
-
-
-  
-  
 
 export default MyApp;
